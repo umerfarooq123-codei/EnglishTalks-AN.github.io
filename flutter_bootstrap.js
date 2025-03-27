ad6a558220b9a9a3c65fe5ec48a3c8db3f829980
Muhammad Umer Farooq <74726720+umerfarooq123-codei@users.noreply.github.com>
2025-03-27 07:02:31 +0500

    checking attendance insertion

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3077932396"
+    serviceWorkerVersion: "1984192183"
   }
 });
